41d8ed0ff3af59f4806aaf50c06d68715cd18465
GILEXX <gilbertosa099@outlook.com>
2020-08-27 15:11:09 -0500

    DOM y Virtual DOM

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Esto no es REACT!
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+// const container = document.getElementById('app');
+// container.appendChild(element);
+
+//Esto es Reac
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, Platzi Badges!</h1>;
+
+const container = document.getElementById('app');
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(element, container);
